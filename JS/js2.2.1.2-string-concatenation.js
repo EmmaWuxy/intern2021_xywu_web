7728cb4c7f2c0a0489f0cebe5dfd6325809607fc
Emma Wu <xinyi.wu@mail.mcgill.ca>
2021-03-24 12:03:43 +0800

    Add js string practices

---
+++ JS/js2.2.1.2-string-concatenation.js
@@ -0,0 +1,13 @@
+// Concatenation
+let one = "Hello ";
+let two = "how are you?";
+let joined = one + two;
+console.log(joined);
+
+// Concatenation of variables and actual strings
+let multiple = one + one + one + "I am fine - " + two;
+console.log(multiple);
+
+//Auto convertion from number to string
+let front = 'Front' + 242;
+console.log(front);
